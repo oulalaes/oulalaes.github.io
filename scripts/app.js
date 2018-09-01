20467744f6784576810d08d91dfdd3c788468597
Aurelie <oulala.pozuelo@gmail.com>
2018-09-01 18:13:06 +0200

    text change

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 /**
  * 
- * @version v1.0.0 - 2018-08-26
+ * @version v1.0.0 - 2018-09-01
  * @link wwww.oulala.es
  * @author 
  This is proprietary software and not allowed for distribution or use without explicit consent.
